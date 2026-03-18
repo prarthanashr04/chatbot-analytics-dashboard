bf4069cfb38ee45fd5a9203743fbabb0993ee3b3
Prarthana Sharma <prarthanashr04@gmail.com>
2026-03-18 13:04:21 +0530

    Setup application routing with lazy loaded feature routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'chatbot',
+        pathMatch: 'full'
+    }, {
+        path: 'chatbot',
+        loadComponent: () => import('./features/chatbot/chatbot').then(c => c.Chatbot)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then(c => c.Dashboard)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./features/not-found/not-found').then(c => c.NotFound)
+    }
+];
